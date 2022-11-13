bb08c8013f9674f70c8eb58b1b46ed2bf4fdcb7f
duekdms <mmp7afy8@naver.com>
2022-11-14 02:51:37 +0900

    어느정도 화긴

--- Insurance_rock_spring/src/main/java/com/mju/spring/DTO/InsuranceDTO.java
+++ Insurance_rock_spring/src/main/java/com/mju/spring/DTO/InsuranceDTO.java
@@ -9,16 +9,19 @@ public class InsuranceDTO {
 	private EInsurance insuranceType;
 	private boolean longTerm;
 	
+
 	public EInsurance getInsuranceType() {
 		return insuranceType;
 	}
 	public void setInsuranceType(EInsurance insuranceType) {
 		this.insuranceType = insuranceType;
 	}
+	
 	public boolean isLongTerm() {
 		return longTerm;
 	}
 	public void setLongTerm(boolean longTerm) {
 		this.longTerm = longTerm;
 	}
+	
 }
--- Insurance_rock_spring/src/main/java/com/mju/spring/Service/InsuranceDesignService.java
+++ Insurance_rock_spring/src/main/java/com/mju/spring/Service/InsuranceDesignService.java
@@ -1,10 +1,11 @@
 package com.mju.spring.Service;
 
+import java.util.Enumeration;
+
 import com.mju.spring.DTO.InsuranceDTO;
 import com.mju.spring.DTO.InsuranceDTO.EInsurance;
 
 public interface InsuranceDesignService {
-
 
 	InsuranceDTO getinsuranceType(EInsurance insuranceType);
 
