712da4336b8e17daa87a92def326a3238b33aa05
ansm6 <ansm6@DESKTOP-KTP9VIM>
2022-11-13 23:46:22 +0900

    11-13 안수민 insuranceDao 추가

---
+++ Insurance_rock_spring/src/main/java/com/mju/spring/DAO/RegisterInsuranceDaoImpl.java
@@ -0,0 +1,5 @@
+package com.mju.spring.DAO;
+
+public class RegisterInsuranceDaoImpl implements RegisterInsuranceDao {
+
+}
--- Insurance_rock_spring/src/main/java/com/mju/spring/Service/InsuranceDesignServiceImpl.java
+++ Insurance_rock_spring/src/main/java/com/mju/spring/Service/InsuranceDesignServiceImpl.java
@@ -1,5 +1,8 @@
 package com.mju.spring.Service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class InsuranceDesignServiceImpl implements InsuranceDesignService {
 
 }
