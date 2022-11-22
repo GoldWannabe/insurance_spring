fd5be6d0af011b42e5730fa38b7b204d9b5d06f5
ansm6 <ansm6@DESKTOP-KTP9VIM>
2022-11-22 22:45:34 +0900

    merge

--- Insurance_rock_spring/src/main/java/com/mju/spring/DAO/TransactionDaoImpl.java
+++ Insurance_rock_spring/src/main/java/com/mju/spring/DAO/TransactionDaoImpl.java
@@ -2,4 +2,10 @@ package com.mju.spring.DAO;
 
 public class TransactionDaoImpl implements TransactionDao {
 
+	@Override
+	public int getTrx_ID() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
 }
