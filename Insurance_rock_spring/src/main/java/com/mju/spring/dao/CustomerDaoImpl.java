1dc3ad8cc01f8c33b06f4b69918329501776e713
ansm6 <ansm6@DESKTOP-KTP9VIM>
2022-11-28 01:00:09 +0900

    merge

--- Insurance_rock_spring/src/main/java/com/mju/spring/dao/CustomerDaoImpl.java
+++ Insurance_rock_spring/src/main/java/com/mju/spring/dao/CustomerDaoImpl.java
@@ -1,9 +1,11 @@
 package com.mju.spring.dao;
 
 import org.apache.ibatis.session.SqlSession;
+import org.springframework.stereotype.Repository;
 
 import com.mju.spring.dto.damageAssessment.compansate.CustomerBankDto;
 
+@Repository
 public class CustomerDaoImpl implements CustomerDao{
 	private SqlSession sqlSession = MyBatisConnectionFactory.getSqlSessionFactory().openSession();
 	
