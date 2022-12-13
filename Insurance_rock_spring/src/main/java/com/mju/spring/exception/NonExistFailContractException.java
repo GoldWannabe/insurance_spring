5759c71bb1f01fca1c083f30e2bd51b1671de127
221B0825 <221B0825eunseoyu0825@gmail.com>
2022-12-13 13:45:31 +0900

    NonExistFailContract Message

--- Insurance_rock_spring/src/main/java/com/mju/spring/exception/NonExistFailContractException.java
+++ Insurance_rock_spring/src/main/java/com/mju/spring/exception/NonExistFailContractException.java
@@ -3,7 +3,7 @@ package com.mju.spring.exception;
 @SuppressWarnings("serial")
 public class NonExistFailContractException extends RuntimeException{
 	public NonExistFailContractException() {		
-		super("해당고객이 가입한 보험이 존재하지않습니다. 다시입력해주세요");
+		super("해당고객의 가입 실패 내역이 존재하지않습니다. 다시입력해주세요");
 	}
 
 }
